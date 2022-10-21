ab29a03f59118a683ae5cb7d97e0672080916803
Edgar <edgarsanou@gmail.com>
2022-10-21 19:22:12 +0200

    Implemented spinner generation with code

--- app/src/main/java/com/evermine/sudokuapp/MainActivity.java
+++ app/src/main/java/com/evermine/sudokuapp/MainActivity.java
@@ -16,9 +16,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        CharSequence[] nombres = {"0","1","2","3","4","5"};
+        Spinner spinnerList[][] = new Spinner[8][8];
+        CharSequence[] nombres = {"1","2","3","4","5","6","7","8","9"};
 
         TableLayout tl = findViewById(R.id.tableLayout);
+        for (int y = 0; y<9; y++){
+            TableRow row = new TableRow(this);
+            for (int x = 0; x<9; x++){
+                Spinner spinner = new Spinner(this);
+                spinner.setBackground(null);
+                spinner.setPadding(5, 5, 5, 5);
+                ArrayAdapter<CharSequence> adapter = new ArrayAdapter<CharSequence>(this, android.R.layout.simple_spinner_item, nombres);
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                spinner.setAdapter(adapter);
+                row.addView(spinner);
+                spinner.setTag(R.id.col,x);
+                spinner.setTag(R.id.fila,y);
+                spinnerList[y][x]=spinner;
+            }
+            tl.addView(row);
+        }
+        /*
         for (int i = 0; i<5; i++){
             TableRow row = new TableRow(this);
             Spinner spinner = new Spinner(this);
@@ -35,6 +53,6 @@ public class MainActivity extends AppCompatActivity {
             row.addView(spinner3);
             tl.addView(row);
         }
-
+        */
     }
 }

1a8d0d1a5d93e99913163f84ed1994bef7d524d2
Edgar <edgarsanou@gmail.com>
2022-11-06 20:40:39 +0100

    Implemented the resolve function with backtracking algorithm and commented all code

--- app/src/main/java/com/evermine/sudokuapp/MainActivity.java
+++ app/src/main/java/com/evermine/sudokuapp/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.SimpleCursorAdapter;
 import android.widget.Spinner;
 import android.widget.TableLayout;
@@ -25,31 +26,49 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Value secuence for the spinner
         CharSequence[] nombres = {"0","1","2","3","4","5","6","7","8","9"};
-
+        //TableLayout
         TableLayout tl = findViewById(R.id.tableLayout);
+        //Button to resolve the Sudoku with backtracking
+        Button resolve = findViewById(R.id.resolve);
+        resolve.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                resolveGame(0,0); //Calling resolveGame Function
+                updateView(); //Updating view
+            }
+        });
+        //Now we add a 9x9 spinner table simulating a sudoku
         for (int y = 0; y<9; y++){
             TableRow row = new TableRow(this);
             for (int x = 0; x<9; x++){
                 Spinner spinner = new Spinner(this);
+                //We need to change spinner design
                 spinner.setBackground(null);
                 spinner.setPadding(5, 5, 5, 5);
+                //Adding the charsecuence values to spinner
                 ArrayAdapter<CharSequence> adapter = new ArrayAdapter<CharSequence>(this, android.R.layout.simple_spinner_item, nombres);
                 adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
                 spinner.setAdapter(adapter);
-                row.addView(spinner);
+                row.addView(spinner); //Adding the spinner to the table
+                //Setting the spinner tags
                 spinner.setTag(R.id.col,x);
                 spinner.setTag(R.id.fila,y);
+                //Adding OnItemSelectedListener on every spinner
                 spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
                     @Override
                     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                        //Getting the row and colum
                         int fila = (int) adapterView.getTag(R.id.col);
                         int col = (int) adapterView.getTag(R.id.fila);
+                        //Trying to set the value to the model and refresh the view
                         if(!sudoku.setSudokuvalue(col, fila, adapterView.getSelectedItemPosition()) && adapterView.getSelectedItemPosition() != 0){
-                            spinnerList[col][fila].setBackgroundResource(R.drawable.spinnerwrong_background);
-                            spinnerList[col][fila].setSelection(sudoku.getSudokuValue(col,fila));
+                            spinnerList[col][fila].setBackgroundResource(R.drawable.spinnerwrong_background);//Setting red background
+                            spinnerList[col][fila].setSelection(sudoku.getSudokuValue(col,fila)); //Updating spinner selection
                         }else{
-                            spinnerList[col][fila].setBackground(null);
+                            spinnerList[col][fila].setBackground(null);//Removing the background
+                            spinnerList[col][fila].setSelection(sudoku.getSudokuValue(col,fila)); //Updating spinner selection
                         }
                     }
 
@@ -58,53 +77,84 @@ public class MainActivity extends AppCompatActivity {
 
                     }
                 });
-                spinnerList[y][x]=spinner;
-                //spinner.setSelection(4);
-                //System.out.println(sudoku.getSudokuValue(y,x));
+                spinnerList[y][x]=spinner;//Adding spinner to spinners Array
             }
-            tl.addView(row);
+            tl.addView(row);//Adding the row to the table
 
         }
-        startGame();
-
+        startGame(); //Now we start the game adding 30 random values
     }
 
+    /*
+    * This method updates all spinners values with the model value
+    *
+     */
     public static void updateView(){
         for (int i = 0;i<9;i++){
             for (int j = 0;j<9;j++){
                 spinnerList[i][j].setSelection(sudoku.getSudokuValue(i,j));
                 spinnerList[i][j].setBackground(null);
-                //spinnerList[i][j].setEnabled(false);
             }
         }
-        sudoku.showSudoku();
     }
 
+    /*
+    * This method starts the game filling 30 values with a random number
+    * from 1 to 9 and check if this value is valid
+     */
     public static void startGame(){
         Random rand = new Random();
-        int number = 15;
+        int number = 30;
         while(number>0){
-            int i = rand.nextInt(9);
-            int j = rand.nextInt(9);
+            int i = rand.nextInt(9); //Random col
+            int j = rand.nextInt(9); //Random row
             System.out.println(i+ "j "+j);
             if(sudoku.setSudokuvalue(i,j,rand.nextInt(9)+1)==true){
                 number--;
-                spinnerList[i][j].setEnabled(false);
+                spinnerList[i][j].setEnabled(false);//Setting spinner disabled
             }else{
+                //If the value is not correct this function tries to change other value
                 sudoku.setSudokuvalue(i,j,0);
             }
         }
-        updateView();
+        updateView();//Updating the view with the predefined values
 
     }
 
-    public static void selectSpinnerItemByValue(Spinner spnr, long value) {
-        SimpleCursorAdapter adapter = (SimpleCursorAdapter) spnr.getAdapter();
-        for (int position = 0; position < adapter.getCount(); position++) {
-            if(adapter.getItemId(position) == value) {
-                spnr.setSelection(position);
-                return;
+    /*
+    * This recursive method resolves the game with backtracking algorithm
+    * @param col This parameter references to column
+    * @param row This parameter references to row
+    * @return boolean This return a boolean var if the value is valid or not
+     */
+
+    public static boolean  resolveGame(int col, int row){
+        //If the row is 9 it means that the sudoku has been solved
+        if(row==9){
+            return true;
+        }
+        //If the col is 9 we need to call the function to resolve the following row
+        else if(col==9){
+            return resolveGame(0,row+1);
+        }
+        //If we detect a value we need to skip this value, becose can be a predefined value
+        //that the game has been generated
+        else if(sudoku.getSudokuValue(col,row)!=0){
+            return resolveGame(col+1,row);
+        }
+        //Now we need to do a for that try to set a value from 1 to 9
+        //until we find the right one
+        for(int i = 1;i<=9;i++){
+            if (sudoku.setSudokuvalue(col, row, i)) {
+                sudoku.showSudoku();
+                if(resolveGame(col+1,row)){
+                    return true;
+                }
             }
         }
+        //If we dont find a valid value we will return false and the previous call will try with the following value
+        sudoku.setSudokuvalue(col, row, 0);
+        return false;
     }
+
 }
--- app/src/main/java/com/evermine/sudokuapp/Sudoku.java
+++ app/src/main/java/com/evermine/sudokuapp/Sudoku.java
@@ -4,6 +4,7 @@ public class Sudoku {
     private int[][] sudoku = new int[9][9];
 
     public Sudoku(){
+        // When we initialize the constructor we fill all the values with 0
         for (int i = 0;i< sudoku.length;i++) {
             for (int j = 0; j < sudoku.length; j++) {
                 sudoku[i][j] = 0;
@@ -16,25 +17,35 @@ public class Sudoku {
         return sudoku[i][j];
     }
 
+    /*
+    * This function tries to set a value and returns if the value is valid or not
+    * @param i References to column
+    * @param j References to row
+    * @return boolean This return a boolean depending if the value is valid or not
+     */
     public boolean setSudokuvalue(int i,int j,int value){
         boolean isCorrect = true;
+        //For iteration that checks every column from the same row
         for (int x = 0; x<sudoku[i].length; x++){
             if (x!=j && value==sudoku[i][x] && sudoku[i][x]!=0){
-                System.out.println(value+" Valor? "+sudoku[i][x]);
-                isCorrect=false;
+                isCorrect=false; //If we detect the same value on a row, we set the value to false
             }
         }
+        //For iteration that checks every row from the same column
         for (int y = 0; y<sudoku.length; y++){
             if (y!= i && value==sudoku[y][j] && sudoku[y][j]!=0){
-                isCorrect=false;
-                System.out.println(value+" Valor? "+sudoku[y][j]);
+                isCorrect=false; //If we detect the same value on a column, we set the value to false
             }
         }
-        sudoku[i][j]=value;
-
+        sudoku[i][j]=value; //Finally we will set the value provided
 
         return isCorrect;
     }
+
+    /*
+     * This function make a print with the actual sudoku values
+     *
+     */
     public void showSudoku(){
         for (int i = 0;i<9;i++){
             for (int j = 0; j<9;j++){
@@ -42,5 +53,6 @@ public class Sudoku {
             }
             System.out.println();
         }
+        System.out.println("------------------------");
     }
 }
